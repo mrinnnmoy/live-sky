7bfbc3d641e96efde1e8a89def9b465345420cfb
mrinnnmoy <mrinmoy202000@gmail.com>
2022-10-24 19:11:59 +0530

    build inputs-section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Inputs from './Components/Inputs';
 import TopButtons from './Components/TopButtons';
 // import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
+      <Inputs />
     </div>
   );
 }
