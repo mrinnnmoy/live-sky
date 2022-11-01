31aff7e5bcf93c3bad060486595e005410d582cd
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-01 20:41:22 +0530

    setup weather-api-request

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import './App.css';
-import Forecast from './Components/Forecast';
-import Inputs from './Components/Inputs';
-import TemperatureAndDetails from './Components/TemperatureAndDetails';
-import TimeAndLocation from './Components/TimeAndLocation';
-import TopButtons from './Components/TopButtons';
+import "./App.css";
+import Forecast from "./Components/Forecast";
+import Inputs from "./Components/Inputs";
+import TemperatureAndDetails from "./Components/TemperatureAndDetails";
+import TimeAndLocation from "./Components/TimeAndLocation";
+import TopButtons from "./Components/TopButtons";
+import getFormattedWeatherData from "./services/weatherService";
 // import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({q: "London"});
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
       <Inputs />
       <TimeAndLocation />
       <TemperatureAndDetails />
-      <Forecast title="Hourly Forecast"/>
-      <Forecast title="Daily Forecast"/>
+      <Forecast title="Hourly Forecast" />
+      <Forecast title="Daily Forecast" />
     </div>
   );
 }
