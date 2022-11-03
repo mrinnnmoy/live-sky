d93ec5e26d2407cdf4bd4d6cb0c341fd29695eae
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-03 12:30:51 +0530

    setup api-accept+location+from-frontend

--- src/App.js
+++ src/App.js
@@ -5,24 +5,45 @@ import TimeAndLocation from "./Components/TimeAndLocation";
 import TemperatureAndDetails from "./Components/TemperatureAndDetails";
 import Forecast from "./Components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
 
 function App() {
+  // To store data after being fetched.
+  const [query, setQuery] = useState({ q: "Berlin" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
 
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({q: "London"});
-    console.log(data);
-  };
+  useEffect(() => {
+    // To check the fetched data in console
+    // const fetchWeather = async () => {
+    //   const data = await getFormattedWeatherData({q: "London"});
+    //   console.log(data);
+    // };
 
-  fetchWeather();
+    // To accept Location from frontend
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    // Every time a new location is given, it fetches new data.
+    fetchWeather();
+  }, [query, units]);
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
       <Inputs />
-      <TimeAndLocation />
-      <TemperatureAndDetails />
-      <Forecast title="Hourly Forecast" />
-      <Forecast title="Daily Forecast" />
+
+      {weather && (
+        <div>
+          <TimeAndLocation />
+          <TemperatureAndDetails />
+          <Forecast title="Hourly Forecast" />
+          <Forecast title="Daily Forecast" />
+        </div>
+      )}
     </div>
   );
 }
