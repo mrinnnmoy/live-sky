223062b95c37177125bd1cd232d00a84d27c4f91
mrinnnmoy <mrinmoy202000@gmail.com>
2022-11-05 14:09:33 +0530

    update Inputs-Searchbar&Icon-functional

--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
 
-function Inputs({setQuery, units, setUnits}) {
+function Inputs({ setQuery, units, setUnits }) {
+  const [city, setCity] = useState("");
 
   const handleUnitsChange = (e) => {
     const selectedUnit = e.currentTarget.name;
     if (units !== selectedUnit) setUnits(selectedUnit);
+  };
+
+  const handleSearchClick = () => {
+    if (city !== "") setQuery({ q: city });
   };
 
 
@@ -14,6 +19,8 @@ function Inputs({setQuery, units, setUnits}) {
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="Search a city ..."
           className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize "
@@ -21,6 +28,7 @@ function Inputs({setQuery, units, setUnits}) {
         <UilSearch
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleSearchClick}
         />
         <UilLocationPoint
           size={25}
